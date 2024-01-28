a06c5ac550957c5883a15276d0eace85812a02c1
Guru-25 <guru@duck.com>
2024-01-29 01:12:07 +0530

    new

--- .mozilla/firefox/arkenfox/user-overrides.js
+++ .mozilla/firefox/arkenfox/user-overrides.js
@@ -1,11 +1,15 @@
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
+user_pref("privacy.clearOnShutdown.history", false); // 2811
 
-/* override recipe: RFP is not for me ***/
-user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 [pointless if not using RFP]
+/* disable RFP letterboxing [FF67+] ***/
+user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
 
-/* override recipe: enable DRM and let me watch videos ***/
-user_pref("media.eme.enabled", true); // 2022
+/* experimental RFP [FF91+] ***/
+user_pref("privacy.resistFingerprinting.exemptedDomains", "web.whatsapp.com"); // 4505
+
+/* enable DNS-over-HTTPS (DoH) [FF60+] ***/
+user_pref("network.trr.mode", 3); // 0710
 
 /* disable saving passwords ***/
 user_pref("signon.rememberSignons", false); // 5003
@@ -13,5 +17,5 @@ user_pref("signon.rememberSignons", false); // 5003
 /* disable location bar suggestion types ***/
 user_pref("browser.urlbar.suggest.topsites", false); // 5010
 
-/* enable DNS-over-HTTPS (DoH) [FF60+] ***/
-user_pref("network.trr.mode", 3); // 0710
+/* enable all DRM content (EME: Encryption Media Extension) ***/
+user_pref("media.eme.enabled", true); // 5508
