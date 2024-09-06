d702fab4c98b1cf8d2caa2028299d2dda61ad897
Guru <guru@duck.com>
2024-09-06 20:53:38 +0530

    new

--- .var/app/org.mozilla.firefox/.mozilla/firefox/aa7th091.arkenfox/user-overrides.js
+++ .var/app/org.mozilla.firefox/.mozilla/firefox/aa7th091.arkenfox/user-overrides.js
@@ -1,15 +1,18 @@
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
-user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128+
 
-/* disable RFP letterboxing [FF67+] ***/
-user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
+/* 0710: enable DNS-over-HTTPS (DoH) [FF60+] ***/
+user_pref("network.trr.mode", 3);
 
-/* enable DNS-over-HTTPS (DoH) [FF60+] ***/
-user_pref("network.trr.mode", 3); // 0710
+/* 2651: disable user interaction for security by always asking where to download ***/
+user_pref("browser.download.useDownloadDir", true);
 
-/* disable saving passwords ***/
-user_pref("signon.rememberSignons", false); // 5003
+/* 5003: disable saving passwords ***/
+user_pref("signon.rememberSignons", false);
 
-/* disable location bar suggestion types ***/
-user_pref("browser.urlbar.suggest.topsites", false); // 5010
+/* 5508: enable all DRM content (EME: Encryption Media Extension) ***/
+user_pref("media.eme.enabled", true);
+
+/* 5010: disable location bar suggestion types ***/
+user_pref("browser.urlbar.suggest.topsites", false);
